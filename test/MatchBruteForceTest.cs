1e49edd9279835483d964a98074cf484a10aa8e6
Cameron Manavian <cameron.manavian@gmail.com>
2015-07-22 20:34:00 -0700

    Pull in skeleton from JS version

--- test/MatchBruteForceTest.cs
+++ test/MatchBruteForceTest.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace Match3PuzzleCsharp
 {
+    [TestFixture]
     class MatchBruteForceTest
     {
+        [Test]
+        public void BubbleSort()
+        {
+            // arrange
+            MatchBruteForce match = new MatchBruteForce();
+            int[,] matrix = {  
+                {1, 1, 1},
+                {2, 3, 1},
+                {5, 4, 1}
+            };
+            int[,] sortedArray;
+
+            // act
+            int[,] result = match.get345();
+
+            // assert
+            Assert.AreEqual(1, sortedArray[0]);
+            Assert.AreEqual(2, sortedArray[1]);
+            Assert.AreEqual(3, sortedArray[2]);
+            Assert.AreEqual(4, sortedArray[3]);
+            Assert.AreEqual(5, sortedArray[4]);
+            Assert.AreEqual(10, sortedArray[9]);
+        }
     }
 }
